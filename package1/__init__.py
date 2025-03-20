cd940f89d8710065540e483852599cd115a381a7
Edison-Melendez <157083694+Edison-Melendez@users.noreply.github.com>
2025-03-20 12:15:54 -0400

    Added folders

---
+++ package1/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 20 12:15:19 2025
+
+@author: Melen
+"""
+
---
+++ package1/controllor/PID.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 20 12:14:54 2025
+
+@author: Melen
+"""
+
+pid
