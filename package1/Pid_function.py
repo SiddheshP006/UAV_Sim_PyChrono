76203d90eb62a290989470b76a8caea064658ae1
Edison-Melendez <157083694+Edison-Melendez@users.noreply.github.com>
2025-03-24 21:37:53 -0400

    OdeInputs and pid function added extra paramter

--- package1/Pid_function.py
+++ package1/Pid_function.py
@@ -9,7 +9,7 @@ class rk4:
   def __init__(self):
     print("Integrator CLASS")
   
-  def rk4singlestep(fun, dt, t0, y0, params):
+  def rk4singlestep(fun, dt, t0, y0, gains_instances,odeInputs):
       """
       This function does a single 4th-order Runge-Kutta integration step.
   
@@ -33,9 +33,9 @@ class rk4:
   
       """
       
-      f1 = fun(t0, y0, params)
-      f2 = fun(t0 + dt /2, y0 + (dt / 2) * f1, params)
-      f3 = fun(t0 + dt /2, y0 + (dt / 2) * f2, params)
-      f4 = fun(t0 + dt, y0 + dt * f3, params)
+      f1 = fun(t0, y0, gains_instances,odeInputs)
+      f2 = fun(t0 + dt /2, y0 + (dt / 2) * f1, gains_instances,odeInputs)
+      f3 = fun(t0 + dt /2, y0 + (dt / 2) * f2, gains_instances,odeInputs)
+      f4 = fun(t0 + dt, y0 + dt * f3, gains_instances,odeInputs)
       yout = y0 + (dt / 6) * (f1 + 2 * f2 + 2 * f3 + f4)
       return yout
--- package1/controller/PID/pid.py
+++ package1/controller/PID/pid.py
@@ -13,10 +13,12 @@ PI = math.pi
 #dddddd
 
 class PID_controller:
-  def __init__(self, gains_instance):
+  def __init__(self, gains_instance,odeInputs):
     print("PID controller CLASS")
+    self.odeInputs= odeInputs
+    self.gains_instance=gains_instance
     
-  def ode(self,ode_instance,y):
+  def ode(self,t,y):
       """
       This function defines the system of equations of the PID CONTROLLER that need to be integrated 
   
@@ -25,14 +27,14 @@ class PID_controller:
       
       # error calculations
       
-      self.translational_position_error = self.translational_position_in_I - self.translational_position_in_I_user
+      self.odeInputs.translational_position_error = self.odeInputs.translational_position_in_I - self.odeInputs.translational_position_in_I_user
       #self.angular_error = np.array([roll - roll_ref, pitch - pitch_ref, (((yaw - yaw_ref + PI) % (2*PI)) - PI)]).reshape(3,1)
-      self.x_tran = np.append(self.translational_position_in_I, self.translational_velocity_in_I, axis=0)
+      self.x_tran = np.append(self.odeInputs.translational_position_in_I, self.odeInputs.translational_velocity_in_I, axis=0)
      
       
       # error calcs
       
-      self.translational_position_error = ode_instance.translational_position_error
+      self.odeInputs.translational_position_error = ode_instance.translational_position_error
       #self.angular_error = odeInput.angular_error
       
       state_phi_ref_diff = y[0:2]
@@ -41,10 +43,10 @@ class PID_controller:
       integral_angular_error = y[7:10]
       
       
-      self.mu_tran = self.gains_instance.mass_total_estimated*(- self.gains_instance.KP_tran * self.translational_position_error 
-                                           - self.gains_instance.KD_tran * (self.translational_velocity_in_I - self.translational_velocity_in_I_user)
+      self.mu_tran = self.gains_instance.mass_total_estimated*(- self.gains_instance.KP_tran * self.odeInputs.translational_position_error 
+                                           - self.gains_instance.KD_tran * (self.odeInputs.translational_velocity_in_I - self.odeInputs.translational_velocity_in_I_user)
                                            - self.gains_instance.KI_tran * integral_position_tracking
-                                           + self.translational_acceleration_in_I_user).reshape(3,1)
+                                           + self.odeInputs.translational_acceleration_in_I_user).reshape(3,1)
       
       self.mu_x = self.mu_tran[0].item()
       self.mu_y = self.mu_tran[1].item()
@@ -52,33 +54,33 @@ class PID_controller:
       
       self.u1 = math.sqrt(self.mu_x ** 2 + self.mu_y ** 2 + (self.gains_instance.mass_total_estimated * self.gains_instance.G_acc - self.mu_z) ** 2)
       
-      calculation_var_A = -(1/self.u1) * (self.mu_x * math.sin(self.yaw_ref) - self.mu_y * math.cos(self.yaw_ref))
-      self.roll_ref = math.atan2(calculation_var_A, math.sqrt(1 - calculation_var_A ** 2))
+      calculation_var_A = -(1/self.u1) * (self.mu_x * math.sin(self.odeInputs.yaw_ref) - self.mu_y * math.cos(self.odeInputs.yaw_ref))
+      self.odeInputs.roll_ref = math.atan2(calculation_var_A, math.sqrt(1 - calculation_var_A ** 2))
       
-      self.pitch_ref = math.atan2(-(self.mu_x * math.cos(self.yaw_ref) + self.mu_y * math.sin(self.yaw_ref)), (self.gains_instance.mass_total_estimated * self.gains_instance.G_acc - self.mu_z))
+      self.odeInputs.pitch_ref = math.atan2(-(self.mu_x * math.cos(self.odeInputs.yaw_ref) + self.mu_y * math.sin(self.odeInputs.yaw_ref)), (self.gains_instance.mass_total_estimated * self.gains_instance.G_acc - self.mu_z))
       
-      internal_state_differentiator_phi_ref_diff = self.gains_instance.A_phi_ref * state_phi_ref_diff + self.gains_instance.B_phi_ref*self.roll_ref
-      internal_state_differentiator_theta_ref_diff = self.gains_instance.A_theta_ref * state_theta_ref_diff + self.gains_instance.B_theta_ref*self.pitch_ref
+      internal_state_differentiator_phi_ref_diff = self.gains_instance.A_phi_ref * state_phi_ref_diff + self.gains_instance.B_phi_ref*self.odeInputs.roll_ref
+      internal_state_differentiator_theta_ref_diff = self.gains_instance.A_theta_ref * state_theta_ref_diff + self.gains_instance.B_theta_ref*self.odeInputs.pitch_ref
 
-      self.roll_ref_dot = np.asarray(self.gains_instance.C_phi_ref*state_phi_ref_diff).item()
-      self.pitch_ref_dot = np.asarray(self.gains_instance.C_theta_ref*state_theta_ref_diff).item()
+      self.odeInputs.roll_ref_dot = np.asarray(self.gains_instance.C_phi_ref*state_phi_ref_diff).item()
+      self.odeInputs.pitch_ref_dot = np.asarray(self.gains_instance.C_theta_ref*state_theta_ref_diff).item()
       
-      self.roll_ref_ddot = np.asarray(self.gains_instance.C_phi_ref*internal_state_differentiator_phi_ref_diff).item()
-      self.pitch_ref_ddot = np.asarray(self.gains_instance.C_theta_ref*internal_state_differentiator_theta_ref_diff).item()
+      self.odeInputs.roll_ref_ddot = np.asarray(self.gains_instance.C_phi_ref*internal_state_differentiator_phi_ref_diff).item()
+      self.odeInputs.pitch_ref_ddot = np.asarray(self.gains_instance.C_theta_ref*internal_state_differentiator_theta_ref_diff).item()
       
-      self.angular_error = np.array([self.roll - self.roll_ref, self.pitch - self.pitch_ref, (((self.yaw - self.yaw_ref + PI) % (2*PI)) - PI)]).reshape(3,1)
+      self.angular_error = np.array([self.odeInputs.roll- self.odeInputs.roll_ref, self.odeInputs.pitch - self.odeInputs.pitch_ref, (((self.odeInputs.yaw - self.odeInputs.yaw_ref + PI) % (2*PI)) - PI)]).reshape(3,1)
       
-      self.angular_position_ref_dot = np.array([self.roll_ref_dot, self.pitch_ref_dot, self.yaw_ref_dot]).reshape(3,1)
-      self.angular_position_ref_ddot = np.array([self.roll_ref_ddot, self.pitch_ref_ddot, self.yaw_ref_ddot]).reshape(3,1)
+      self.angular_position_ref_dot = np.array([self.odeInputs.roll_ref_dot, self.odeInputs.pitch_ref_dot, self.odeInputs.yaw_ref_dot]).reshape(3,1)
+      self.angular_position_ref_ddot = np.array([self.odeInputs.roll_ref_ddot, self.odeInputs.pitch_ref_ddot, self.odeInputs.yaw_ref_ddot]).reshape(3,1)
       
-      Jacobian_matrix_inverse = np.matrix([[1, (math.sin(self.roll)*math.sin(self.pitch))/math.cos(self.pitch), (math.cos(self.roll)*math.sin(self.pitch))/math.cos(self.pitch)],
-                                           [0,                                             math.cos(self.roll),                                            -math.sin(self.roll)],
-                                           [0,                        math.sin(self.roll)/math.cos(self.pitch),                       math.cos(self.roll)/math.cos(self.pitch)]])
+      Jacobian_matrix_inverse = np.matrix([[1, (math.sin(self.odeInputs.roll)*math.sin(self.odeInputs.pitch))/math.cos(self.odeInputs.pitch), (math.cos(self.odeInputs.roll)*math.sin(self.odeInputs.pitch))/math.cos(self.odeInputs.pitch)],
+                                           [0,                                             math.cos(self.odeInputs.roll),                                            -math.sin(self.odeInputs.roll)],
+                                           [0,                        math.sin(self.odeInputs.roll)/math.cos(self.odeInputs.pitch),                       math.cos(self.odeInputs.roll)/math.cos(self.odeInputs.pitch)]])
   
-      self.angular_position_dot = Jacobian_matrix_inverse * self.angular_velocity
+      self.angular_position_dot = Jacobian_matrix_inverse * self.odeInputs.angular_velocity
       self.angular_error_dot = self.angular_position_dot - self.angular_position_ref_dot
       
-      self.Moment = np.cross(self.angular_velocity.ravel(), (self.gains_instance.I_matrix_estimated * self.angular_velocity).ravel()).reshape(3,1) 
+      self.Moment = np.cross(self.odeInputs.angular_velocity.ravel(), (self.gains_instance.I_matrix_estimated * self.odeInputs.angular_velocity).ravel()).reshape(3,1) 
       + self.I_matrix_estimated * (- self.gains_instance.KP_rot * self.angular_error
                                    - self.gains_instance.KD_rot * self.angular_error_dot
                                    - self.gains_instance.KI_rot * integral_angular_error
